3d6adaddd9ccd1bfa5aa2ac6303575ca97eb70c2
dcf <duanchifei@doodlemobile.com>
2021-04-06 20:12:27 +0800

    修改自带命令风格
    eg.
    new-item => New-Item

--- custom-function.ps1
+++ custom-function.ps1
@@ -1,5 +1,6 @@
-function createFile { new-item $args -itemtype "file" }
-function createDir { new-item $args -itemtype "directory"}
-function manl() { man -online @args }
-function cdcmd($cmdname) { (gcm $cmdname).path | split-path -parent | cd }
-function ofcmd($cmdname) { (gcm $cmdname).path | split-path -parent | invoke-item }
+function createFile { New-Item $args -itemtype "file" }
+function createDir { New-Item $args -itemtype "directory"}
+function manl() { Get-Help -online @args }
+function cdcmd($cmdname) { (Get-Command $cmdname).path | Split-Path -parent | Set-Location }
+function ofcmd($cmdname) { (Get-Command $cmdname).path | Split-Path -parent | Invoke-Item }
+function github() { Start-Process www.github.com }
