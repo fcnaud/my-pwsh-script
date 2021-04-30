35b9da8aca879ea4ffaf06fb11fedbba91cfc188
dcf <dcf_xyz@outlook.com>
2021-04-30 20:49:03 +0800

    添加 create link 命令

--- custom-function.ps1
+++ custom-function.ps1
@@ -1,5 +1,18 @@
 function CreateFile { New-Item $args -itemtype "file" }
 function CreateDir { New-Item $args -itemtype "directory"}
+function CreateLink
+{
+    Param($Path, $Target, [switch]$S, [switch]$D, [switch]$H, [switch]$J)
+
+    if($S -or $D) {
+        New-Item -ItemType SymbolicLink -Path $Path -Target $Target
+    } elseif($H) {
+        New-Item -ItemType HardLink -Path $Path -Target $Target
+    } elseif($J) {
+        New-Item -ItemType Junction -Path $Path -Target $Target
+    }
+}
+
 function GetOnlineHelp() { Get-Help -online @args }
 function CdCmdDirectory($cmdname) { (Get-Command $cmdname).path | Split-Path -parent | Set-Location }
 function OpenCmdDirectory($cmdname) { (Get-Command $cmdname).path | Split-Path -parent | Invoke-Item }
--- set-alias.ps1
+++ set-alias.ps1
@@ -1,5 +1,7 @@
 set-alias touch CreateFile
 set-alias mkdir CreateDir
+set-alias ln CreateLink
+
 set-alias ccm CdCmdDirectory
 set-alias ocm OpenCmdDirectory
 set-alias manl GetOnlineHelp
